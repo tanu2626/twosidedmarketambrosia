c0b2ff64f66d83274a3a06dd9ff37a8b88da4bbc
Tanu <tanuuu.21@gmail.com>
2016-12-09 23:00:04 +1100

    Made the first change

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
 
   get 'dashboard' => "pages#dashboard"
 
+
+  post '/buy/:slug', to: 'transactions#create', as: :buy
+  get '/pickup/:guid', to: 'transactions#pickup', as: :pickup
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
