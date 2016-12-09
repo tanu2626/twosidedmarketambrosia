cbc218cb881ce4771b8b3bf5428182168b1326dc
Tanu <tanuuu.21@gmail.com>
2016-12-09 17:32:14 +1100

    Added Slugs to Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
+  belongs_to :user
+
   mount_uploader :image, ProductImageUploader
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209042339) do
+ActiveRecord::Schema.define(version: 20161209054931) do
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +34,8 @@ ActiveRecord::Schema.define(version: 20161209042339) do
     t.datetime "updated_at",   null: false
     t.integer  "user_id"
     t.string   "image"
+    t.string   "slug"
+    t.index ["slug"], name: "index_products_on_slug", unique: true
   end
 
   create_table "users", force: :cascade do |t|
